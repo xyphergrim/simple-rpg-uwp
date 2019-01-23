ca24b0b164d801d8521025842806647507308be8
John <johnclancy@outlook.com>
2019-01-23 11:47:29 -0600

    Added GameSession.cs and Player.cs

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "George";
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.HitPoints = 14;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 10;
+        }
+    }
+}
